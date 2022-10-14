5b5070c4ed05f8bde2ab1574f565603d76c32634
Greg <gonzagonz2021@gmail.com>
2022-10-14 18:45:37 -0500

    funcion recursiva 2

--- funcionrecursiva.dart
+++ funcionrecursiva.dart
@@ -1,11 +1,14 @@
+// Aplicación que implementa el factorial de un número
 void main() {
   //Factorial 5 = 5 * 4 * 3 * 2 * 1 = 120
-  int result = calcularFactorial(5);
+ 
+int result = calcularFactorial(5);
   print(result);
   result = calcularFactorial2(4);
   print(result);
 }
 
+//función factorial recursiva
 int calcularFactorial(int numero) {
   if (numero <= 0) {
     return 1;

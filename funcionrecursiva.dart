f8af2f90163171187ba32d4bafc1dd0ba3fa5a7f
Greg <gonzagonz2021@gmail.com>
2022-10-14 18:53:23 -0500

    funcion recursiva 3

--- funcionrecursiva.dart
+++ funcionrecursiva.dart
@@ -10,18 +10,29 @@ int result = calcularFactorial(5);
 
 //función factorial recursiva
 int calcularFactorial(int numero) {
-  if (numero <= 0) {
+  if (numero < 0) {
     return 1;
   } else {
     int result = (numero * calcularFactorial(numero - 1));
     return result;
   }
 }
-
+//función recursiva iterativa
 int calcularFactorial2(int numero) {
   int result = 1;
   for (int i = 1; i <= numero; i++) {
     result *= i;
+  }
+  return result;
+}
+
+//función recursiva iterativa 2
+int calcularFactorial3(int numero) {
+  int result = 1;
+int i = 1;  
+while ( i <= numero ) {
+    result *= i;
+i++;
   }
   return result;
 }
